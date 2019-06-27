590076713a148523c44ae40a4398283328bd9746
Camryn Phillips <clp3ef@virginia.edu>
2019-06-27 11:40:13 -0400

    make_TZR_toa added to AbsPhase (calculates TZRMJD if none given), timing_model updated to work with make_TZR_toa, testing with fit_ files and pintk

--- pint/models/timing_model.py
+++ pint/models/timing_model.py
@@ -508,6 +508,16 @@ class TimingModel(object):
                 for pf in self.phase_funcs:
                     tz_phase += Phase(pf(tz_toa, tz_delay))
                 return phase - tz_phase
+                #if no absolute phase (TZRMJD), add the component to the model and calculate it
+                from pint.models import absolute_phase
+                self.add_component(absolute_phase.AbsPhase())
+                self.make_TZR_toa(toas)#needs timfile to get all toas, but model doesn't have access to timfile. different place for this? 
+            tz_toa = self.get_TZR_toa(toas)
+            tz_delay = self.delay(tz_toa)
+            tz_phase = Phase(np.zeros(len(toas.table)) , np.zeros(len(toas.table)))
+            for pf in self.phase_funcs:
+                tz_phase += Phase(pf(tz_toa, tz_delay))
+            return phase - tz_phase
         else:
             return phase
 
