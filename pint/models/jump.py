32a986b50b0e302ff89bb4eb2e35dc8ad90becf2
Camryn Phillips <clp3ef@virginia.edu>
2019-08-15 08:45:22 -0400

    final cleanup, pintk works

--- pint/models/jump.py
+++ pint/models/jump.py
@@ -24,7 +24,6 @@ class DelayJump(DelayComponent):
     #    self.category = 'delay_jump'
 
     def setup(self):
-        print('in Delayjump setup')
         super(DelayJump, self).setup()
         self.jumps = []
         for mask_par in self.get_params_of_type('maskParameter'):
@@ -68,14 +67,12 @@ class PhaseJump(PhaseComponent):
     """
     register = True
     def __init__(self):
-        print('in Phasejump __init__')
         super(PhaseJump, self).__init__()
         self.add_param(p.maskParameter(name = 'JUMP', units='second'))
         self.phase_funcs_component += [self.jump_phase,]
         self.category = 'phase_jump'
 
     def setup(self):
-        print('in Phasejump setup')
         super(PhaseJump, self).setup()
         self.jumps = []
         for mask_par in self.get_params_of_type('maskParameter'):
@@ -92,8 +89,6 @@ class PhaseJump(PhaseComponent):
         jump parameters. The phase value is determined by jump parameter times
         F0.
         """
-        print('in jump phase')
-        #print('jumps', self.jumps)
         tbl = toas.table
         jphase = numpy.zeros(len(tbl)) * (self.JUMP1.units * self.F0.units)
         for jump in self.jumps:
--- pint/toa.py
+++ pint/toa.py
@@ -598,7 +598,6 @@ class TOAs(object):
 
     def get_groups(self, gap_limit=0.0833):
         if hasattr(self, "toas") or gap_limit != 0.0833:
-            print("calculating groups")
             gap_limit *= u.d
             mjd_dict = OrderedDict()
             for i in np.arange(len(self.get_mjds())):
@@ -630,7 +629,8 @@ class TOAs(object):
         else:
             return self.table['groups']
         
-    def get_highest_density_range(self, nbins=10):
+    def get_highest_density_range(self, ndays=7):
+        nbins = int((max(self.get_mjds()) - min(self.get_mjds()))/(ndays*u.d))
         a = np.histogram(self.get_mjds(), nbins)
         maxday = int(a[1][np.argmax(a[0])])
         diff = int(a[1][1]-a[1][0])
