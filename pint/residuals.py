7a3d7749c2592b45ca46481405616dd40b0eabf9
Camryn Phillips <clp3ef@virginia.edu>
2019-08-16 17:33:12 -0400

    pulse_number --> pulse_numbers, re-dded tracking to residuals

--- pint/residuals.py
+++ pint/residuals.py
@@ -38,12 +38,32 @@ class resids(object):
             delta_pulse_numbers = Phase(self.toas.table['delta_pulse_numbers'])
         full = Phase(np.zeros_like(rs.frac), rs.frac) + delta_pulse_numbers
         full = full.int + full.frac
-
+        
+        #Track on pulse numbers, if necessary
+        if getattr(self.model, 'TRACK').value == '-2':
+            pulse_num = self.toas.get_pulse_numbers()
+            if pulse_num is None:
+                log.error('No pulse numbers with TOAs using TRACK -2')
+                raise Exception('No pulse numbers with TOAs using TRACK -2')
+            
+            pn_act = np.trunc(full)
+            addPhase = pn_act - pulse_num
+            full -= pn_act
+            full += addPhase
+            
+            if not weighted_mean:
+                full -= full.mean()
+            else:
+                w = 1.0 / (np.array(self.toas.get_errors())**2)
+                wm = (full*w).sum() / w.sum()
+                full -= wm
+            return full
+        
         if not weighted_mean:
             full -= full.mean()
         else:
-        # Errs for weighted sum.  Units don't matter since they will
-        # cancel out in the weighted sum.
+            # Errs for weighted sum.  Units don't matter since they will
+            # cancel out in the weighted sum.
             if np.any(self.toas.get_errors() == 0):
                 raise ValueError('TOA errors are zero - cannot calculate residuals')
             w = 1.0/(np.array(self.toas.get_errors())**2)
