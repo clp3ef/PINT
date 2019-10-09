69d690eb8a4f3d7e9936c8b06f5d17e81e935354
Camryn Phillips <clp3ef@virginia.edu>
2019-10-09 15:58:31 -0400

    Ftest and closest_group implemented

--- pint/auto_fit.py
+++ pint/auto_fit.py
@@ -57,8 +57,8 @@ def get_closest_group(all_toas, fit_toas):
         return all_toas.table['groups'][0]    
     
 datadir = os.path.dirname(os.path.abspath(str(__file__)))
-parfile = os.path.join(datadir, 'alg_test.par')
-timfile = os.path.join(datadir, 'alg_test.tim')
+parfile = os.path.join(datadir, 'Ter5G.par')
+timfile = os.path.join(datadir, 'Ter5G.tim')
 
 # read in the initial model
 m = mb.get_model(parfile)
@@ -72,31 +72,20 @@ t.print_summary()
 # These are pre-fit residuals
 rs = pint.residuals.resids(t, m).phase_resids
 xt = t.get_mjds()
-#plt.plot(xt, rs, 'x', label = 'pre-fit')
-#plt.title("%s Pre-Fit Timing Residuals" % m.PSR.value)
-#plt.xlabel('MJD')
-#plt.ylabel('Residual (phase)')
-#plt.grid()
-#plt.show()
 
-#read in the tim and parfiles
-#start with groups - single observations - choose the highest density group and start there
-#select only those toas and fit for f0
-#plot the results
 
-#convert jump flags to params
-#m.jump_flags_to_params(t)
-
-#select highest density group
-print(t.table['groups'])
-#t.select(0 == t.get_groups())
-t.select(0 <= t.get_groups())
-t.select(2 > t.get_groups())
+#starting toas, should not break up groups
+groups = t.get_groups()
+print(groups)
+#a = np.logical_or(groups == 8, groups == 7)
+a = np.logical_and(t.get_mjds() > 53802*u.d, t.get_mjds() < 53804*u.d)
+print(a)
+t.select(a)
 print(t.table['groups'])
 
-last_chi2 = 2000000000000000
 cont = True
 while cont:
+    do_ftest = True
     # Now do the fit
     print("Fitting...")
     f = pint.fitter.WlsFitter(t, m)
@@ -112,59 +101,29 @@ while cont:
     full_groups = pint.toa.get_TOAs(timfile).table['groups']
     selected = [True if group in t.table['groups'] else False for group in full_groups] 
     rs_mean = pint.residuals.resids(pint.toa.get_TOAs(timfile), f.model, set_pulse_nums=True).phase_resids[selected].mean()
-    f_toas, rss, rmods = pint.random_models.random_models(f, rs_mean, iter=12, ledge_multiplier=0.2)
+    f_toas, rss, rmods = pint.random_models.random_models(f, rs_mean, iter=12, ledge_multiplier=1, redge_multiplier=2)
     
     t_others = pint.toa.get_TOAs(timfile)
     
     print('rs_mean',rs_mean)
     print(t.table['groups'])
-    num = max(t.table['groups'])
-    print('num', num)
     closest_group = get_closest_group(deepcopy(t_others), deepcopy(t))
     print('closest_group',closest_group, type(closest_group))
     #get closest group, t_others is t plus that group
-    #t_others.select(t_others.get_groups() > num-4)
-    t_others.select(t_others.get_groups() < num+2)
+    #right now t_others is just all the toas, so can use as all
+    a = np.logical_or(a, t_others.get_groups() == closest_group)
+    t_others.select(a)
+    print(t_others.table['groups'])
     
     print('0 model chi2', f.resids.chi2_reduced)
     print('0 model chi2_ext', pint.residuals.resids(t_others, f.model).chi2_reduced)
     
-    if f.resids.chi2_reduced > 1.5*last_chi2:
-        print('last chi2 0 model was', last_chi2)
-        print('chi2 is going up, add a new parameter?')
-        last_chi2 = 20000000000000
-        f_params = []
-        for param in m.params:
-            if getattr(m, param).frozen == False:
-                f_params.append(param)
-        print(f_params)
-        if 'RAJ' not in f_params:
-            #add RAJ
-            getattr(m, 'RAJ').frozen = False
-        elif 'DECJ' not in f_params:
-            #add decj
-            getattr(m, 'DECJ').frozen = False
-        elif 'F1' not in f_params:
-            #add F1
-            getattr(m, 'F1').frozen = False
-        else:
-            print('F0, RAJ, DECJ, and F1 all added')
-            cont = False
-        continue
-
     for i in range(len(rmods)):
-        #chi2_1 = pint.residuals.resids(t, f.model).chi2
-        #dof_1 = pint.residuals.resids(t, f.model).dof
-        #chi2_2 = pint.residuals.resids(t_others, rmods[i]).chi2
-        #dof_2 = pint.residuals.resids(t_others, rmods[i]).dof
-        #print(chi2_1,dof_1,chi2_2,dof_2)
-        #print('F-test',pu.Ftest(chi2_1, dof_1, chi2_2, dof_2))
         print('chi2 reduced',pint.residuals.resids(t, rmods[i]).chi2_reduced)
         print('chi2 reduced ext', pint.residuals.resids(t_others, rmods[i]).chi2_reduced)
         plt.plot(f_toas, rss[i], '-', alpha=0.6)
     
     print(f.get_fitparams().keys())
-    t.unselect()
     t.unselect()
     
     #plot post fit residuals with error bars
@@ -177,53 +136,56 @@ while cont:
     plt.ylabel('Residual (us)')
     r = pint.residuals.resids(t,m).time_resids.to(u.us).value
     plt.ylim(min(r)-200,max(r)+200)
+    #plt.xlim?
     plt.legend()
     plt.grid()
     plt.show()
     
+    #get next model by comparing chi2 for t_others
     chi2_ext = [pint.residuals.resids(t_others, rmods[i]).chi2_reduced.value for i in range(len(rmods))]
     chi2_dict = dict(zip(chi2_ext, rmods))
+    #append 0model to dict so it can also be a possibility
+    chi2_dict[pint.residuals.resids(t_others, f.model).chi2_reduced.value] = f.model
     min_chi2 = sorted(chi2_dict.keys())[0]
-    if f.resids.chi2_reduced > 0:
-        #prevent 2 points - F0 from messing it up
-        last_chi2 = f.resids.chi2_reduced
     
-    if min_chi2 < pint.residuals.resids(t_others, f.model).chi2_reduced.value:
-        #replace current model with better model
-        m = chi2_dict[min_chi2]
-        t.select(t.get_groups() >= 0)
-        t.select(t.get_groups() < num + 2)
+    #m is new model
+    m = chi2_dict[min_chi2]
+    #a = current t plus closest group, defined above
+    t.select(a)
+        
+    #use Ftest to decide whether to add a parameter to new model
+    #new model = m
+    #new model + new param = m_plus
+    m_plus = deepcopy(m)
+    f_params = []
+    for param in m.params:
+        if getattr(m, param).frozen == False:
+            f_params.append(param)
+    if 'RAJ' not in f_params:
+        #add RAJ
+        getattr(m_plus, 'RAJ').frozen = False
+    elif 'DECJ' not in f_params:
+        #add decj
+        getattr(m_plus, 'DECJ').frozen = False
+    elif 'F1' not in f_params:
+        #add F1
+        getattr(m_plus, 'F1').frozen = False
     else:
-        print('no better models, add a new parameter?')
-        last_chi2 = 20000000000000
-        f_params = []
-        for param in m.params:
-            if getattr(m, param).frozen == False:
-                f_params.append(param)
-        print(f_params)
-        if 'RAJ' not in f_params:
-            #add RAJ
-            getattr(m, 'RAJ').frozen = False
-        elif 'DECJ' not in f_params:
-            #add decj
-            getattr(m, 'DECJ').frozen = False
-        elif 'F1' not in f_params:
-            #add F1
-            getattr(m, 'F1').frozen = False
-        else:
-            print('F0, RAJ, DECJ, and F1 all added')
-            #cont = False
-
-    
-    #for key in sorted(chi2_dict.keys()):
-    #    m = chi2_dict[key]
-    #    plt.plot(t.get_mjds(), pint.residuals.resids(t,m).time_resids.to(u.us).value, '.', label = 'post-fit')
-    #    plt.title(key)
-    #    plt.xlabel('MJD')
-    #    plt.ylabel('Residual (us)')
-    #    plt.legend()
-    #    plt.grid()
-    #    plt.show()
-    
-                                                            
-                                                            
+        print('F0, RAJ, DECJ, and F1 all added')
+        do_ftest = False
+        #cont = False
+    if do_ftest:
+        #actually fit with new param over extended points
+        f = pint.fitter.WlsFitter(t, m)
+        f.fit_toas()
+        f_plus = pint.fitter.WlsFitter(t, m_plus)
+        f_plus.fit_toas()
+        #compare m and m_plus
+        m_rs = pint.residuals.resids(t, f.model)
+        m_plus_rs = pint.residuals.resids(t, f_plus.model)
+        print(m_rs.chi2, m_rs.dof, m_plus_rs.chi2, m_plus_rs.dof)
+        Ftest = pu.Ftest(float(m_rs.chi2.value), m_rs.dof, float(m_plus_rs.chi2.value), m_plus_rs.dof)
+        print(Ftest)
+        if Ftest < 0.0005:
+            #say model is model_plus (AKA, add the parameter)
+            m = deepcopy(m_plus)
